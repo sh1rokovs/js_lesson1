44cb7f303cb693ac5932a1cd6d1a442f37e20936
sh1rokovs <hilallod@yandex.ru>
2020-09-03 11:24:22 +0300

    less5

--- script_2.js
+++ script_2.js
@@ -1,23 +1,89 @@
-var form = document.forms.regform;
-var btnNextEx = document.getElementById('next-ex');
-var boxNextEx = document.getElementsByClassName('box-next-ex');
+'use strict';
+// Фэйк АПИ
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-var textBefore = document.getElementById('text-box__before-text'); 
-var textAfter = document.getElementById('text-box__after-text'); 
+let app = new Vue ({
+    el: '#app',
+    mounted: function() {
+        this.filteredProducts = this.products;
+    },
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [{
+                  "id_product": 123,
+                  "product_name": "Монитор",
+                  "price": 45600
+                },
+                {
+                    "id_product": 124,
+                    "product_name": "Монитор",
+                    "price": 45600
+                  },
+                {
+                  "id_product": 456,
+                  "product_name": "Мышка",
+                  "price": 1000
+                },
+                {
+                    "id_product": 457,
+                    "product_name": "Мышка беспроводная",
+                    "price": 1000
+                  },
+                {
+                    "id_product": 458,
+                    "product_name": "Ноутбук",
+                    "price": 45600
+                  },    
+            ],
+        imgCatalog: 'http://placehold.it/250x150',//заглушка
+        searchText: '',
+        filteredProducts: []
+        
+    },
 
-var openNextEx = () => {
-    form.style.display = 'none';
-    boxNextEx[0].style.display = 'block';
-
-};
-
-var changeText = () => {
-    let str = textBefore.textContent;
-    console.log(str);
-
-    textAfter.innerText = str.replace(/^'|(\s)'|'(\s)|'$/g, '$1"$2');
     
-    
-    //вариант учителя
-    //textAfter.innerText = str.replace(/\B'|'\B/g, '"');
-};
+    methods: {
+        addProduct (product) {
+            console.log (product.id_product);
+        },
+
+        searchFor () {
+            let text = this.searchText.toLowerCase().trim();
+            
+            if (text === '') {
+                this.filteredProducts = this.products;
+            } else {
+                this.filteredProducts = this.products.filter((el) => {
+                    return el.product_name.toLowerCase().includes(text);
+                });
+            }
+            // console.log(this.searchText);
+            // console.log(this.products.length);
+            
+            // let findproduct = () => {
+            //     console.log('start Find product');
+
+            //     let productIsHere =  false;
+            //     for (let el of this.products) {
+                    
+            //         if (this.searchText == el.product_name) {
+            //             productIsHere = true;
+            //             break;
+            //         } else {
+            //             productIsHere = false;;
+            //         } 
+                    
+            //     } return productIsHere;
+            // }
+            // if (findproduct() == true) {
+            //     console.log();
+                
+            //     alert('Есть такой');
+            // } else {
+            //     alert('НЕЕЕЕЕЕ найден');
+            // }
+        }
+    },
+})
+
+
