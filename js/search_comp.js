3ab74354e400add0421577b70a6fed3592606358
sh1rokovs <hilallod@yandex.ru>
2020-09-11 14:22:36 +0300

    lesson7

--- js/search_comp.js
+++
@@ -1,8 +0,0 @@
-Vue.component('searchform', {
-    props: ['value'],
-    template: `
-        <input type="text" class="search-field" 
-        v-bind:value="value"
-        v-on:input="$emit('input', $event.target.value)">
-    `
-})
